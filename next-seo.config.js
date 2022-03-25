c6dca6b2419fb439ef9c00906a4fce9dbc670487
lesrz <suarezarnaldi.leo@gmail.com>
2022-03-25 20:08:21 -0300

    chore: edit default template root files

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,25 +1,21 @@
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title: "nextarter-chakra",
-  titleTemplate: "%s | nextarter-chakra",
-  defaultTitle: "nextarter-chakra",
-  description: "Next.js + chakra-ui + TypeScript template",
-  canonical: "https://nextarter-chakra.sznm.dev",
+  title: "LostArkly",
+  titleTemplate: "%s | LostArkly",
+  defaultTitle: "LostArkly",
+  description: "Keep track of your Lost Ark daily & weekly tasks.",
+  canonical: "",
   openGraph: {
-    url: "https://nextarter-chakra.sznm.dev",
-    title: "nextarter-chakra",
-    description: "Next.js + chakra-ui + TypeScript template",
+    url: "",
+    title: "LostArkly",
+    description: "Keep track of your Lost Ark daily & weekly tasks.",
     images: [
       {
-        url: "https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250",
-        alt: "nextarter-chakra.sznm.dev og-image",
+        url: "",
+        alt: "",
       },
     ],
-    site_name: "nextarter-chakra",
-  },
-  twitter: {
-    handle: "@sozonome",
-    cardType: "summary_large_image",
+    site_name: "lostarkly",
   },
 };
 
